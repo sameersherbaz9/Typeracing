050f44844edc76134ab9321b277014281b0c5d46
Sameer Sherbaz <sameersherbaz9@gmail.com>
2026-06-14 23:22:22 +0500

    Add finishRace route to race.js

--- backend/routes/race.js
+++ backend/routes/race.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middleware/auth');
-const { createRace, joinRace, getRaceResult } = require('../controllers/raceController');
+const { createRace, joinRace, getRaceResult, finishRace } = require('../controllers/raceController');
 
 router.post('/create', authMiddleware, createRace);
 router.post('/join', authMiddleware, joinRace);
 router.get('/result', authMiddleware, getRaceResult);
+router.post('/finish', authMiddleware, finishRace);
 
 module.exports = router;
