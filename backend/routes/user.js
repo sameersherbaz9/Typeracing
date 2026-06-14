e41c96317942bcf4468232f5c63065b58f877549
Sameer Sherbaz <sameersherbaz9@gmail.com>
2026-06-14 23:49:07 +0500

    Update user.js

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middleware/auth');
-const { getProfile, getRaceHistory } = require('../controllers/userController');
+const { getProfile, getRaceHistory, getWpmStats } = require('../controllers/userController');
 
 router.get('/profile', authMiddleware, getProfile);
 router.get('/race-history', authMiddleware, getRaceHistory);
+router.get('/wpm-stats', authMiddleware, getWpmStats);
 
 module.exports = router;
