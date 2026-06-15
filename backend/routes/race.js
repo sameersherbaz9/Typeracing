d6741b233d4e5884192cebed143ab2ce92b6858e
Sameer Sherbaz <sameersherbaz9@gmail.com>
2026-06-15 05:17:51 +0500

    Add abandonRace route to race.js

--- backend/routes/race.js
+++ backend/routes/race.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middleware/auth');
-const { createRace, joinRace, getRaceResult, finishRace } = require('../controllers/raceController');
+const { createRace, joinRace, getRaceResult, finishRace, abandonRace } = require('../controllers/raceController');
 
 router.post('/create', authMiddleware, createRace);
 router.post('/join', authMiddleware, joinRace);
 router.get('/result', authMiddleware, getRaceResult);
 router.post('/finish', authMiddleware, finishRace);
+router.post('/abandon', authMiddleware, abandonRace);
 
 module.exports = router;
